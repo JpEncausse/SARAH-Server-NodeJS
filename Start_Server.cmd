898f8621073783dc0963755a1b1db2f36b49d7a1
Jean-Philippe Encausse <Jp@encausse.net>
2016-01-07 20:02:09 +0100

    Fix changelog.txt and gitignore

--- Start_Server.cmd
+++
@@ -1,8 +0,0 @@
-SET PATH_NODEJS=%cd%\server\NodeJS
-SET PATH_EXPRESS=%cd%\server\ExpressJS
-SET PATH_PHANTOM=%cd%\server\PhantomJS
-SET PATH_SARAH=%cd%\server\app
-SET PATH_PLUGINS=%cd%\plugins
-
-SET NODE_PATH=%PATH_NODEJS%\node_modules\;%PATH_EXPRESS%\node_modules\;%PATH_SARAH%\node_modules\;
-"%PATH_NODEJS%\node.exe" %PATH_SARAH%/app.js
